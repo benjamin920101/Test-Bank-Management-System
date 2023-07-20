ef6fa65a25c11ea2a6caa23058e6839508989d9b
benjamin920101 <47590892+benjamin920101@users.noreply.github.com>
2023-07-20 11:04:09 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,3 @@
+explorer https://github.com/benjamin920101/Test-Bank-Management-System/archive/refs/heads/main.zip
+powershell -command "Expand-Archive Test-Bank-Management-System-main.zip"
+winget install Python.Python.3.11
